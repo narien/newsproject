732b9fd33373fb134083a1d1092b7f66cb1d1c8d
fredrk <fredrk>
2015-03-24 22:23:18 +0100

    Lade till grunden till databasen.

---
+++ clientserver/databasemem.h
@@ -0,0 +1,17 @@
+//Database on memeory
+
+#ifndef DATABASEMEM_H
+#define DATABASEMEM_H
+
+#include <map>
+
+using namespace std;
+
+class DatabaseMem {
+	public:
+
+	private:
+		map<int idNumber map<int idNumber, articleFields af> storage;
+
+};
+#endif

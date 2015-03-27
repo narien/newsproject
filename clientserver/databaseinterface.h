11865f326504ba2aadff721fd13d0fd06be913d2
fredrk <fredrk>
2015-03-27 11:25:14 +0100

    updated database test

--- clientserver/databaseinterface.h
+++ clientserver/databaseinterface.h
@@ -4,11 +4,10 @@
 #define DATABASE_INTERFACE_H
 
 #include "newsgroup.h"
-//#include "protocol.h"
 
 #include <string>
-#include <utility>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
@@ -19,7 +18,7 @@ class DatabaseInterface {
 		virtual bool removeNewsgroup(int& newsgroup_id) = 0;
 		virtual int removeArticle(int& newsgroup_id, int& article_id) = 0; //returns 1 if it worked, 0 if no such newsgroup and -1 if no such article
     	virtual vector<pair<int, string>> listNewsgroups() = 0;
-    	virtual bool listArticles(int& newsgroup_id, vector<pair<int, string>>& articles) = 0;
+    	virtual bool listArticles(int& newsgroup_id, vector<pair<int, string>>& articles) = 0; //articles -> <article_id, article_title>
     	virtual int getArticle(const int& newsgroup_id, const int& article_id, string& article_title, string& article_author, string& article_text) = 0; //returns 1 if it worked, 0 if no such newsgroup and -1 if no such article, writes the relevant data to the input variables
 };
 #endif
--- clientserver/testdatabase.cc
+++ clientserver/testdatabase.cc
@@ -30,19 +30,39 @@ int main() {
 	cout << endl;
 
 	int a = 1;
+	int b = 2;
 	string text1 = "text1";
 	string text2 = "text2";
 
 
-	cout << dbmem.insertArticle(a, "atitle1", "author1", text1);
+	dbmem.insertArticle(a, "atitle1", "author1", text1);
 	dbmem.insertArticle(a, "atitle2", "author2", text2);
+	dbmem.insertArticle(a, "atitle3", "author3", text2);
+	dbmem.insertArticle(a, "atitle4", "author4", text2);
 
-	cout << "Articles in Newsgroup 1:" << endl;
+	dbmem.removeArticle(a, b);
+
+
+	cout << "Articles in Newsgroup " << a << ":" << endl;
 	vector<pair<int, string>> v;
 	dbmem.listArticles(a, v);
 	for (auto i : v) {
 		cout << "id: " << i.first << " title: " << i.second << endl;
 	}
+
+	cout << endl << "Get Article: " <<endl;
+
+	string getTitle;
+	string getAuthor;
+	string getText;
+
+	int c = 4;
+
+	dbmem.getArticle(a, c, getTitle, getAuthor, getText);
+
+	cout << "id: " << b << " Title: " << getTitle << " Author: " << getAuthor << " Text: " << getText << endl;
+
+	cout << endl;
 
 	cout << "TEST DISK DATABASE" << endl << endl;
 
